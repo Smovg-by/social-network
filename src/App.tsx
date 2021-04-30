171ac595f0cbefe97c4108597c28914bec15417e
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-04-30 11:54:41 +0300

    feat: HTML markup divide into React components

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,16 @@
 import React from 'react';
 import './App.css';
+import {Header} from './components/Header';
+import {Navbar} from './components/Navbar';
+import {Profile} from './components/Profile';
+
 
 function App() {
   return (
     <div className="app-wrapper">
-      <header className="header">
-        <img
-          src="https://www.phpro.be/uploads/media/sulu-100x100/00/440-react%404x.png?v=2-0"
-          alt="logo"/>
-      </header>
-      <nav className="nav">
-        <div><a href="#">Profile</a></div>
-        <div><a href="#">Messages</a></div>
-        <div><a href="#">News</a></div>
-        <div><a href="#">Music</a></div>
-        <div><a href="#">Settings</a></div>
-      </nav>
-      <div className="content">
-
-        <div>
-          <img
-            src="https://i.siteapi.org/no2IMQtVA-Jko-HOxCRaQpEC3FE=/fit-in/900x1000/center/top/filters:format(png)/5045b586a05f134.s.siteapi.org/img/d35c0d1ac7c8dd12e1e4146f14df612b9c31a4a3.jpg"
-            alt="nature"/>
-        </div>
-        <div>Ava + description</div>
-        <div>My posts
-          <div>New post</div>
-          <div>
-            <div>Post1</div>
-            <div>Post2</div>
-          </div>
-        </div>
-      </div>
+      <Header/>
+      <Navbar/>
+      <Profile/>
     </div>
   );
 }
