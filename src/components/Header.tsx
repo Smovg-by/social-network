a11de0f21f555eb739dd99320e9b84e43475fc3d
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-04-30 11:54:41 +0300

    Lesson 12. HTML markup turn into React components

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export function Header() {
+  return (
+    <header className="header">
+      <img
+        src="https://www.phpro.be/uploads/media/sulu-100x100/00/440-react%404x.png?v=2-0"
+        alt="logo"/>
+    </header>
+  )
+}
+
---
+++ src/components/Profile.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export function Profile() {
+  return (
+    <div className="content">
+      <div>
+        <img
+          src="https://i.siteapi.org/no2IMQtVA-Jko-HOxCRaQpEC3FE=/fit-in/900x1000/center/top/filters:format(png)/5045b586a05f134.s.siteapi.org/img/d35c0d1ac7c8dd12e1e4146f14df612b9c31a4a3.jpg"
+          alt="nature"/>
+      </div>
+      <div>Ava + description</div>
+      <div>My posts
+        <div>New post</div>
+        <div>
+          <div>Post1</div>
+          <div>Post2</div>
+        </div>
+      </div>
+    </div>
+  )
+}
