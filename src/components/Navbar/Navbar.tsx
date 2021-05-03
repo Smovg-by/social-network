333c5e67b6f3a9afebe9f2244ea928f80230b613
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-05-03 17:48:17 +0300

    lesson 19. Route, BrowserRouter

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import classes from './Navbar.module.css';
 
-console.log(classes)
-
 export function Navbar() {
   return (
     <nav className={classes.nav}>
-      <div className={classes.item}><a>Profile</a></div>
-      <div className={classes.item}><a>Messages</a></div>
-      <div className={classes.item}><a>News</a></div>
-      <div className={classes.item}><a>Music</a></div>
-      <div className={classes.item}><a>Settings</a></div>
+      <div className={classes.item}><a
+        href="/profile">Profile</a></div>
+      <div className={classes.item}><a
+        href="/dialogs">Messages</a></div>
+      <div className={classes.item}><a href="/News">News</a>
+      </div>
+      <div className={classes.item}><a
+        href="/Music">Music</a></div>
+      <div className={classes.item}><a
+        href="/Settings">Settings</a></div>
     </nav>
   )
 }
