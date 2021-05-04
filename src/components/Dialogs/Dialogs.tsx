0a24f900f7384c32d282be7f037b80480aa6fa87
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-05-04 12:10:04 +0300

    lesson 21. Create Dialogs page CS HTML

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,6 +2,36 @@ import classes from './Dialogs.module.css'
 
 export function Dialogs() {
   return (
-    <div>Dialogs</div>
+    <div className={classes.dialogs}>
+      <div className={classes.dialogsItems}>
+        <div className={classes.dialog  + ' ' + classes.active}>
+          Dimych
+        </div>
+        <div className={classes.dialog}>
+          Andrey
+        </div>
+        <div className={classes.dialog}>
+          Sveta
+        </div>
+        <div className={classes.dialog}>
+          Sasha
+        </div>
+        <div className={classes.dialog}>
+          Viktor
+        </div>
+        <div className={classes.dialog}>
+          Valera
+        </div>
+      </div>
+      <div className={classes.messages}>
+        <div className={classes.message}>Hi</div>
+        <div className={classes.message}>How is your
+          ITkamasutra
+        </div>
+        <div className={classes.message}>Yo!</div>
+
+
+      </div>
+    </div>
   )
 }
