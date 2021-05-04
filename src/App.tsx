9c37fcafede9a475c0d87dadbbf6a21e5a2abaac
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-05-04 12:43:14 +0300

    lesson 22. Get informed about route exact

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
         <Navbar/>
         {/*<Profile/>*/}
         <div className="app-wrapper-content">
-          <Route path='/dialogs' component={Dialogs}/>
+          <Route exact path='/dialogs' component={Dialogs}/>
           <Route path='/profile' component={Profile}/>
           <Route path='/news' component={News}/>
           <Route path='/music' component={Music}/>
--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,26 +1,34 @@
 import classes from './Dialogs.module.css'
+import {NavLink} from 'react-router-dom';
 
 export function Dialogs() {
   return (
     <div className={classes.dialogs}>
       <div className={classes.dialogsItems}>
-        <div className={classes.dialog  + ' ' + classes.active}>
-          Dimych
+        <div
+          className={classes.dialog + ' ' + classes.active}>
+          <NavLink
+            to="/dialogs/1">Dimych</NavLink>
         </div>
         <div className={classes.dialog}>
-          Andrey
+          <NavLink
+            to="/dialogs/2">Andrey</NavLink>
         </div>
         <div className={classes.dialog}>
-          Sveta
+          <NavLink
+            to="/dialogs/3">Sveta</NavLink>
         </div>
         <div className={classes.dialog}>
-          Sasha
+          <NavLink
+            to="/dialogs/4">Sasha</NavLink>
         </div>
         <div className={classes.dialog}>
-          Viktor
+          <NavLink
+            to="/dialogs/5">Victor</NavLink>
         </div>
         <div className={classes.dialog}>
-          Valera
+          <NavLink
+            to="/dialogs/6">Valera</NavLink>
         </div>
       </div>
       <div className={classes.messages}>
