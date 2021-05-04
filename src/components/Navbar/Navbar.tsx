511410a4ff41c6260da884efa461579253560ab6
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-05-04 11:16:30 +0300

    lesson 20. Use NavLink to='' and activeClassName='classes.activeLink' attribute

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import classes from './Navbar.module.css';
+import {NavLink} from 'react-router-dom';
 
 export function Navbar() {
   return (
     <nav className={classes.nav}>
-      <div className={classes.item}><a
-        href="/profile">Profile</a></div>
-      <div className={classes.item}><a
-        href="/dialogs">Messages</a></div>
-      <div className={classes.item}><a href="/News">News</a>
+      <div className={classes.item}>
+        <NavLink to="/profile"
+                 activeClassName={classes.activeLink}>Profile</NavLink>
       </div>
-      <div className={classes.item}><a
-        href="/Music">Music</a></div>
-      <div className={classes.item}><a
-        href="/Settings">Settings</a></div>
+      <div className={classes.item}>
+        <NavLink to="/dialogs"
+                 activeClassName={classes.activeLink}>Messages</NavLink>
+      </div>
+      <div className={classes.item}>
+        <NavLink to="/News"
+                 activeClassName={classes.activeLink}>News</NavLink>
+      </div>
+      <div className={classes.item}>
+        <NavLink to="/Music"
+                 activeClassName={classes.activeLink}>Music</NavLink>
+      </div>
+      <div className={classes.item}>
+        <NavLink to="/Settings"
+                 activeClassName={classes.activeLink}>Settings</NavLink>
+      </div>
     </nav>
   )
 }
