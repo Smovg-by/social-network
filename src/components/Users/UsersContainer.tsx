ef70f735b2fa1ebfa7ff19f360afc3dfc9db9d22
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-07-03 19:12:24 +0300

    lesson 56. Presentational and Container component

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,6 +1,5 @@
 import { connect } from 'react-redux'
 import { RootStateType } from '../../redux/redux-store'
-import { Users } from './Users'
 import {
   followAC,
   setCurrentPageAC,
@@ -10,6 +9,57 @@ import {
   UserType
 } from '../../redux/usersReducer'
 import { Dispatch } from 'redux'
+import axios from 'axios'
+import React from 'react'
+import { Users } from './Users'
+
+type UsersAPIPropsType = {
+  users: Array<UserType>
+  pageSize: number
+  totalUsersCount: number
+  currentPage: number
+  setUsers: (items: Array<UserType>) => void
+  follow: (id: number) => void
+  unfollow: (id: number) => void
+  setCurrentPage: (currentPage: number) => void
+  setTotalUsersCount: (totalCount: number) => void
+}
+
+class UsersContainer extends React.Component<UsersAPIPropsType> {
+
+  componentDidMount() {
+    axios
+      .get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+      .then(response => {
+        this.props.setUsers(response.data.items)
+        this.props.setTotalUsersCount(response.data.totalCount)
+      })
+  }
+
+  onPageChanged = (page: number) => {
+
+    this.props.setCurrentPage(page)
+    axios
+      .get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`)
+      .then(response => {
+        this.props.setUsers(response.data.items)
+      })
+  }
+
+  render() {
+
+    return <Users
+
+      totalUsersCount={this.props.totalUsersCount}
+      pageSize={this.props.pageSize}
+      currentPage={this.props.currentPage}
+      onPageChanged={this.onPageChanged}
+      users={this.props.users}
+      follow={this.props.follow}
+      unfollow={this.props.unfollow}
+    />
+  }
+}
 
 export type UsersPropsType = MapStatePropsType & mapDispatchPropsType
 
@@ -57,7 +107,7 @@ let mapDispatchToProps = (dispatch: Dispatch): mapDispatchPropsType => {
   }
 }
 
-export const UsersContainer = connect(
+export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(Users)
+)(UsersContainer)
